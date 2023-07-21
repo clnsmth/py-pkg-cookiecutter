ac917cdd980f0b1f00dea3317504082bb5b9c48c
Colin Smith <colin.smith@wisc.edu>
2023-07-21 16:35:40 -0400

    Add missing docstring for pylint

--- {{ cookiecutter.__package_slug }}/tests/test_{{ cookiecutter.__package_slug }}.py
+++ {{ cookiecutter.__package_slug }}/tests/test_{{ cookiecutter.__package_slug }}.py
@@ -1,12 +1,13 @@
 """For testing the {{ cookiecutter.__package_slug }} module."""
 
-import pytest
 from src.{{ cookiecutter.__package_slug }}.{{ cookiecutter.__package_slug }} import hello_world
 
 
 def test_test():
+    """Test test"""
     assert 1 == 1
 
 
 def test_hello_world():
+    """Test hello_world"""
     assert hello_world("Hello world", False) == "Hello world"
